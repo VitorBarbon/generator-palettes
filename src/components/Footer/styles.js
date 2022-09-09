938672a23a4ad3a2046c04a064005384d1082670
Vitor_Barbon <88788433+VitorBarbon@users.noreply.github.com>
2022-09-09 12:41:10 -0300

    Update styles.js

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components';
 export const FooterContainer = styled.footer`
   ${({ theme }) => css`
     display: flex;
-    flex-grow: 1;
     width: 100%;
     height: 70px;
     background-color: ${theme.colors.primaryColor};
