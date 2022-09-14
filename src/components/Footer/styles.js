c094294bcda6bbf5f0418bfc1a250a255e339fda
Vitor_Barbon <88788433+VitorBarbon@users.noreply.github.com>
2022-09-14 16:03:53 -0300

    Update styles.js

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,7 +4,7 @@ export const FooterContainer = styled.footer`
   ${({ theme }) => css`
     display: flex;
     width: 100%;
-    height: 70px;
+    height: 100%;
     background-color: ${theme.colors.primaryColor};
   `}
 `;
